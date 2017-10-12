b391c38de1a3a7c934d6a28beddab94ede8a61ac
Joseph Robinson <smartech56@gmail.com>
2017-10-12 16:28:55 -0700

    added getscripts.sh

---
+++ getscripts.sh
@@ -0,0 +1,21 @@
+echo "preparing..."
+mkdir Scripts
+cd Scripts
+echo "Downloading/Updating Scritps."
+wget https://buzzzy.co/Hub/ServerSetup/Scripts/spigot.sh
+wget https://buzzzy.co/Hub/ServerSetup/Scripts/Bungee.sh
+wget https://buzzzy.co/Hub/ServerSetup/Scripts/Waterfall.sh
+wget https://buzzzy.co/Hub/ServerSetup/Scripts/Lilypad.sh
+wget https://buzzzy.co/Hub/ServerSetup/Scripts/Vanilla.sh
+wget https://buzzzy.co/Hub/ServerSetup/Scripts/Paperspigot.sh
+wget https://buzzzy.co/Hub/ServerSetup/Global/basics.sh
+wget https://buzzzy.co/Hub/ServerSetup/Scripts/Forge.sh
+wget https://buzzzy.co/Hub/ServerSetup/Scripts/Cauldron.sh
+wget https://buzzzy.co/Hub/ServerSetup/Scripts/Sponge.sh
+wget https://buzzzy.co/Hub/ServerSetup/Scripts/web.sh
+wget https://buzzzy.co/Hub/ServerSetup/Scripts/Travertine.sh
+wget https://buzzzy.co/Hub/ServerSetup/Scripts/TacoSpigot.sh
+wget https://buzzzy.co/Hub/ServerSetup/Scripts/HexaCord.sh
+wget https://buzzzy.co/Hub/ServerSetup/Scripts/craftbukkit.sh
+wget https://buzzzy.co/Hub/ServerSetup/Scripts/Torch.sh
+wget https://buzzzy.co/Hub/ServerSetup/Scripts/Hose.sh
