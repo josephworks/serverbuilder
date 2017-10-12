f3ceb7e5a07a80b1e721ca584d71950e05684b7b
Joseph Robinson <smartech56@gmail.com>
2017-10-12 15:46:07 -0700

    removed broken commands

---
+++ buildsteps.sh
@@ -0,0 +1,6 @@
+# every command i did to create serverbuilder
+cd ./serverbuilder
+ls
+sudo apt update
+sudo apt upgrade
+./serverbuilder.sh
--- serverbuild.sh
+++ serverbuild.sh
@@ -1,12 +1,13 @@
 echo starting...
 echo installing dependencies
-sudo apt-get update
+sudo apt update
 sudo apt install toilet figlet maven default-jre
 echo showing start message...
 toilet -f mono12 -F metal Server
 toilet -f mono12 -F metal Builder
-echo creating custom commands
-echo %commandhere% > /usr/bin/commands.sh
+# echo creating custom commands
+# broken for now
+# echo %commandhere% > /usr/bin/commands.sh
 echo "do /help for help"
 echo ┍━━━━━━━━━━━━━┑
 echo Serverbuilder>
