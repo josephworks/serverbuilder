9199a968b1d20bae1c6061cb663c4e80fc2606a9
Joseph Robinson <smartech56@gmail.com>
2017-10-14 14:55:09 -0700

    edited wordpress module

--- ServerSetup-Core/Backend/wordpress.sh
+++ ServerSetup-Core/Backend/wordpress.sh
@@ -1,39 +1,17 @@
 ##################################################################################
 ##                 SERVER SETUP SCRIPT CREATED BY EMERALDTNT                    ##
 ##                 Report errors: https://github.com/Beags/ServerSetup/issues   ##
-##                 Report Spigot/Bungee errors https://goo.gl/w9aoZT            ##
-##################################################################################
 ##     All credit for the items installed/downloaded go to original authors     ##
 ##################################################################################
 #!/bin/sh
-echo "
-   ▄████████    ▄████████    ▄████████  ▄█    █▄     ▄████████    ▄████████         ▄████████    ▄████████ ███    █▄      ███        ▄███████▄ 
-  ███    ███   ███    ███   ███    ███ ███    ███   ███    ███   ███    ███        ███    ███   ███    ███ ███    ███ ▀█████████▄   ███    ███ 
-  ███    █▀    ███    █▀    ███    ███ ███    ███   ███    █▀    ███    ███        ███    █▀    ███    █▀  ███    ███    ▀███▀▀██   ███    ███ 
-  ███         ▄███▄▄▄      ▄███▄▄▄▄██▀ ███    ███  ▄███▄▄▄      ▄███▄▄▄▄██▀        ███         ▄███▄▄▄     ███    ███     ███   ▀   ███    ███ 
-▀███████████ ▀▀███▀▀▀     ▀▀███▀▀▀▀▀   ███    ███ ▀▀███▀▀▀     ▀▀███▀▀▀▀▀        ▀███████████ ▀▀███▀▀▀     ███    ███     ███     ▀█████████▀  
-         ███   ███    █▄  ▀███████████ ███    ███   ███    █▄  ▀███████████               ███   ███    █▄  ███    ███     ███       ███        
-   ▄█    ███   ███    ███   ███    ███ ███    ███   ███    ███   ███    ███         ▄█    ███   ███    ███ ███    ███     ███       ███        
- ▄████████▀    ██████████   ███    ███  ▀██████▀    ██████████   ███    ███       ▄████████▀    ██████████ ████████▀     ▄████▀    ▄████▀      
-                            ███    ███                           ███    ███                                                                    
-
-Version: 5.5
-"
+echo "Version: 5.5"
 sleep 2
-echo "
-Created By EmeraldTnt
-"
+echo "Created By EmeraldTnt"
 sleep .3
-echo "
-Report all bugs here: https://github.com/Beags/ServerSetup/issues
-"
+echo "Report all bugs here: https://github.com/Beags/ServerSetup/issues"
 sleep 1
-echo "
-Installing wordpress
-"
-echo "
-What should the wordpress database password be?
-"
+echo "Installing wordpress"
+echo "What should the wordpress database password be?"
 read -r Passwordvar
 mysql -u root -p -e 'create database wordpress;'
 mysql -u root -p -e 'create user wordpress;'
@@ -43,37 +21,21 @@ cd /var/www/html
 wget https://wordpress.org/latest.zip
 unzip latest.zip
 mv wordpress/* .
-echo "
-What should your first unqiue secret key be?
-"
+echo "What should your first unqiue secret key be?"
 read -r Firstsecretvar
-echo "
-What should your second unqiue secret key be?
-"
+echo "What should your second unqiue secret key be?"
 read -r Secondsecretvar
-echo "
-What should your third unqiue secret key be?
-"
+echo "What should your third unqiue secret key be?"
 read -r thirdsecretvar
-echo "
-What should your fourth unqiue secret key be?
-"
+echo "What should your fourth unqiue secret key be?"
 read -r Fourthsecretvar
-echo "
-What should your fifth unqiue secret key be?
-"
+echo "What should your fifth unqiue secret key be?"
 read -r Fifthsecretvar
-echo "
-What should your sixth unqiue secret key be?
-"
+echo "What should your sixth unqiue secret key be?"
 read -r Sixthsecretvar
-echo "
-What should your Seventh unqiue secret key be?
-"
+echo "What should your Seventh unqiue secret key be?"
 read -r Seventhsecretvar
-echo "
-What should your eighth unqiue secret key be?
-"
+echo "What should your eighth unqiue secret key be?"
 read -r Eighthecretvar
 cat > wp-config.php << END_TEXT
 <?php
@@ -167,5 +129,4 @@ if ( !defined('ABSPATH') )
 require_once(ABSPATH . 'wp-settings.php');
 
 END_TEXT
-echo "
-Main install done navagate your browser to: http://yourip/ "
+echo "Main install done navagate your browser to: http://yourip/ "
