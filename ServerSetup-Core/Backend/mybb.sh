0445844f71fd6fbc69eb3dd77c4d53568da72e32
Joseph Robinson <smartech56@gmail.com>
2017-10-14 14:48:33 -0700

    edited backend\mybb.sh

--- ServerSetup-Core/Backend/mybb.sh
+++ ServerSetup-Core/Backend/mybb.sh
@@ -1,39 +1,17 @@
 ##################################################################################
 ##                 SERVER SETUP SCRIPT CREATED BY EMERALDTNT                    ##
 ##                 Report errors: https://github.com/Beags/ServerSetup/issues   ##
-##                 Report Spigot/Bungee errors https://goo.gl/w9aoZT            ##
-##################################################################################
 ##     All credit for the items installed/downloaded go to original authors     ##
 ##################################################################################
 #!/bin/sh
-echo "
-   ▄████████    ▄████████    ▄████████  ▄█    █▄     ▄████████    ▄████████         ▄████████    ▄████████ ███    █▄      ███        ▄███████▄ 
-  ███    ███   ███    ███   ███    ███ ███    ███   ███    ███   ███    ███        ███    ███   ███    ███ ███    ███ ▀█████████▄   ███    ███ 
-  ███    █▀    ███    █▀    ███    ███ ███    ███   ███    █▀    ███    ███        ███    █▀    ███    █▀  ███    ███    ▀███▀▀██   ███    ███ 
-  ███         ▄███▄▄▄      ▄███▄▄▄▄██▀ ███    ███  ▄███▄▄▄      ▄███▄▄▄▄██▀        ███         ▄███▄▄▄     ███    ███     ███   ▀   ███    ███ 
-▀███████████ ▀▀███▀▀▀     ▀▀███▀▀▀▀▀   ███    ███ ▀▀███▀▀▀     ▀▀███▀▀▀▀▀        ▀███████████ ▀▀███▀▀▀     ███    ███     ███     ▀█████████▀  
-         ███   ███    █▄  ▀███████████ ███    ███   ███    █▄  ▀███████████               ███   ███    █▄  ███    ███     ███       ███        
-   ▄█    ███   ███    ███   ███    ███ ███    ███   ███    ███   ███    ███         ▄█    ███   ███    ███ ███    ███     ███       ███        
- ▄████████▀    ██████████   ███    ███  ▀██████▀    ██████████   ███    ███       ▄████████▀    ██████████ ████████▀     ▄████▀    ▄████▀      
-                            ███    ███                           ███    ███                                                                    
-
-Version: 5.5
-"
+echo "Version: 5.5"
 sleep 2
-echo "
-Created By EmeraldTnt
-"
+echo "Created By EmeraldTnt"
 sleep .3
-echo "
-Report all bugs here: https://github.com/Beags/ServerSetup/issues
-"
+echo "Report all bugs here: https://github.com/Beags/ServerSetup/issues"
 sleep 1
-echo "
-Installing mybb
-"
-echo "
-What should the mybb database password be?
-"
+echo "Installing mybb"
+echo "What should the mybb database password be?"
 read -r Passwordvar
 mysql -u root -p -e 'create database mybb;'
 mysql -u root -p -e 'create user mybb;'
@@ -48,10 +26,6 @@ chmod 666 inc/config.php inc/settings.php
 chmod 777 cache/ cache/themes/ uploads/ uploads/avatars/
 chmod 666 inc/languages/english/*.php inc/languages/english/admin/*.php
 chmod 777 cache/ cache/themes/ uploads/ uploads/avatars/ admin/backups/
-echo "
-Main install done navagate your browser to: http://yourip/install 
-"
-echo "
-Your MYSQl Info: databasename: mybb user: mybb password: $Passwordvar
-"
+echo "Main install done navagate your browser to: http://yourip/install "
+echo "Your MYSQl Info: databasename: mybb user: mybb password: $Passwordvar"
 sleep 2
