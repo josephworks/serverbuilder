dec952e264e2d0c12800e21bc88ed84f701cf7f7
Joseph Robinson <smartech56@gmail.com>
2017-10-14 11:45:42 -0700

    forked basucs.sh

--- ServerSetup-Core/MCM/Scripts/basics.sh
+++ ServerSetup-Core/MCM/Scripts/basics.sh
@@ -1,59 +1,50 @@
-##                 SERVER SETUP SCRIPT CREATED BY EMERALDTNT                    ##
-##                 Report config error https://buzzzy.co                         ##
-##                 Report Spigot errors https://goo.gl/w9aoZT                   ##
 ##################################################################################
-##           All credit for the plugins downloaded go to their authors          ##
+##                    SERVER SETUP MODULE SCRIPT BY EMERALDTNT                  ##
+##      Report errors: https://github.com/josephworks/serverbuilder/issues      ##
+##     All credit for the items installed/downloaded go to original authors     ##
 ##################################################################################
 #!/bin/sh
-echo "ServerBuilder 3.0 by EmeraldTnt"
+toilet -f mono12 -F metal Server
+toilet -f mono12 -F metal Builder
+echo "Version: 2.0"
+sleep 2
+echo "Written by EmeraldTnt"
+sleep .3
+echo "Report all bugs here: https://github.com/josephworks/serverbuilder/issues"
 sleep 1
-cd /etc/apt/sources.list.d/
-cat > java-8-debian.list << END_TEXT
-deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main
-deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main
-END_TEXT
 PS3='Choose your linux distro: '
 options=("Debian" "Ubuntu" "Centos" )
 select opt in "${options[@]}"
 do
     case $opt in
         "Debian")
-            apt-get update
-            apt-get install wget
-            apt-get install apache2
-            apt-get install mysql-server
-            apt-get install php5 libapache2-mod-php5
+            sudo apt update
+            sudo apt install wget mysql-server default-jre screen git php5-common libapache2-mod-php5 php5-cli apache2
             /etc/init.d/apache2 restart
-            apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
-            apt-get update
-            apt-get install oracle-java8-installer
-            apt-get update
-            apt-get install screen
-            apt-get install git
+            service apache2 restart
             echo "Basics installed, you can go back to the main script and install the server now!"
             exit
             ;;
          "Ubuntu")
-            apt-get update
-            apt-get install wget
-            apt-get install apache2
-            apt-get install mysql-server
-            apt-get install php5 libapache2-mod-php5
+            sudo apt update
+            sudo apt install wget apache2 mysql-server php5 libapache2-mod-php5 default-jre screen git
             /etc/init.d/apache2 restart
-            cd /etc/apt/sources.list.d/
-            wget https://buzzzy.co/Hub/serversetup/Debian/java-8-debian.list
-            apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
-            apt-get update
-            apt-get install oracle-java8-installer
-            apt-get update
-            apt-get install screen
-            apt-get install git
             echo "Basics installed, you can go back to the main script and install the server now!"
             exit
             ;;
          "Centos")
             yum update
             yum install wget
+            yum install httpd
+            systemctl start httpd.service
+            systemctl enable httpd.service
+            yum install mariadb-server mariadb
+            systemctl start mariadb
+            systemctl enable mariadb.service
+            mysql_secure_installation
+            yum install php php-mysql
+            yum -y install php-gd php-ldap php-odbc php-pear php-xml php-xmlrpc php-mbstring php-snmp php-soap curl curl-devel
+            systemctl restart httpd.service
             cd /opt/
             wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u111-b14/jdk-8u111-linux-x64.tar.gz"
             tar xzf jdk-8u111-linux-x64.tar.gz
