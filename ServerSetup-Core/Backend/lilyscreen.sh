c45d0f9646bafdb220fe3f770775ac2609085726
Joseph Robinson <smartech56@gmail.com>
2017-10-14 14:52:37 -0700

    edited lilyscreen

--- ServerSetup-Core/Backend/lilyscreen.sh
+++ ServerSetup-Core/Backend/lilyscreen.sh
@@ -1,32 +1,14 @@
 ##################################################################################
 ##                 SERVER SETUP SCRIPT CREATED BY EMERALDTNT                    ##
 ##                 Report errors: https://github.com/Beags/ServerSetup/issues   ##
-##                 Report Spigot/Bungee errors https://goo.gl/w9aoZT            ##
-##################################################################################
 ##     All credit for the items installed/downloaded go to original authors     ##
 ##################################################################################
 #!/bin/sh
-echo "
-   ▄████████    ▄████████    ▄████████  ▄█    █▄     ▄████████    ▄████████         ▄████████    ▄████████ ███    █▄      ███        ▄███████▄ 
-  ███    ███   ███    ███   ███    ███ ███    ███   ███    ███   ███    ███        ███    ███   ███    ███ ███    ███ ▀█████████▄   ███    ███ 
-  ███    █▀    ███    █▀    ███    ███ ███    ███   ███    █▀    ███    ███        ███    █▀    ███    █▀  ███    ███    ▀███▀▀██   ███    ███ 
-  ███         ▄███▄▄▄      ▄███▄▄▄▄██▀ ███    ███  ▄███▄▄▄      ▄███▄▄▄▄██▀        ███         ▄███▄▄▄     ███    ███     ███   ▀   ███    ███ 
-▀███████████ ▀▀███▀▀▀     ▀▀███▀▀▀▀▀   ███    ███ ▀▀███▀▀▀     ▀▀███▀▀▀▀▀        ▀███████████ ▀▀███▀▀▀     ███    ███     ███     ▀█████████▀  
-         ███   ███    █▄  ▀███████████ ███    ███   ███    █▄  ▀███████████               ███   ███    █▄  ███    ███     ███       ███        
-   ▄█    ███   ███    ███   ███    ███ ███    ███   ███    ███   ███    ███         ▄█    ███   ███    ███ ███    ███     ███       ███        
- ▄████████▀    ██████████   ███    ███  ▀██████▀    ██████████   ███    ███       ▄████████▀    ██████████ ████████▀     ▄████▀    ▄████▀      
-                            ███    ███                           ███    ███                                                                    
-
-Version: 5.5
-"
+echo "Version: 5.5"
 sleep 2
-echo "
-Created By EmeraldTnt
-"
+echo "Created By EmeraldTnt"
 sleep .3
-echo "
-Report all bugs here: https://github.com/Beags/ServerSetup/issues
-"
+echo "Report all bugs here: https://github.com/Beags/ServerSetup/issues"
 sleep .2
 PS3='Choose your linux distro: '
 options=("Debian" "Ubuntu" "Centos" )
@@ -38,14 +20,12 @@ do
             wget https://buzzzy.co/ServerSetup/Global/lilyrestart.sh
             chmod +x proxy-linux-amd64
             screen -dmS Proxy sh
-            screen -S Proxy -X stuff "./proxy-linux-amd64
-            "
+            screen -S Proxy -X stuff "./proxy-linux-amd64"
             cd ..
             cd ~/Connect
             chmod +x connect-linux-amd64
             screen -dmS Connect sh
-            screen -S Connect -X stuff "./connect-linux-amd64
-            "
+            screen -S Connect -X stuff "./connect-linux-amd64"
             echo "Lilypad has now been installed, it is not setup so you will need to do it yourself, if you would like it setup automatically buy the upgrade! It setups bungee, waterfall, and lilypad networks!"
             sleep 2
             ;;
@@ -67,14 +47,12 @@ do
             wget https://buzzzy.co/ServerSetup/Global/lilyrestart.sh
             chmod +x proxy-linux-amd64
             screen -dmS Proxy sh
-            screen -S Proxy -X stuff "./proxy-linux-amd64
-            "
+            screen -S Proxy -X stuff "./proxy-linux-amd64"
             cd ..
             cd ~/Connect
             chmod +x connect-linux-amd64
             screen -dmS Connect sh
-            screen -S Connect -X stuff "./connect-linux-amd64
-            "
+            screen -S Connect -X stuff "./connect-linux-amd64"
             echo "Lilypad has now been installed, it is not setup so you will need to do it yourself, if you would like it setup automatically buy the upgrade! It setups bungee, waterfall, and lilypad networks!"
             sleep 2
             ;;
